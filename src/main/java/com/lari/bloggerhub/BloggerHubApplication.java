6a4ddb0542fc9fd4564c26f6e26ff4f3ee8eb198
amaanlari <amaanlari@outlook.comm>
2024-11-17 02:08:32 +0530

    initial commit

---
+++ src/main/java/com/lari/bloggerhub/BloggerHubApplication.java
@@ -0,0 +1,13 @@
+package com.lari.bloggerhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lari/bloggerhub/BloggerHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lari.bloggerhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
