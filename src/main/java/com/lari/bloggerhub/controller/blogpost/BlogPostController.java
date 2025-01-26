06ca34259458f0783770b720f20cf982e7b3173a
Amaan Lari <121742871+amaanlari@users.noreply.github.com>
2025-01-27 01:39:55 +0530

    Blogging feature/blog post features (#9)
    * Add fields mapping and timestamps update logic
    
    Mapped database fields using @Field annotations in BlogUser and added logic to update timestamps upon modifications. Introduced a new endpoint to fetch all user details and updated role-based access for endpoints.
    
    * Implement blog post module with CRUD operations
    
    - Create `BlogPost` document schema class
    - Implement `BlogPostController` with CRUD endpoints
    - Develop `BlogPostService` for business logic
    - Set up `BlogPostRepository` for data access
    - Add DTOs for blog post requests and responses
    - Include premium status field in blog post schema
    - Update the jackson naming property to snake case
    
    * Update README file
    
    - Add README file for the GitHub repo
    
    ---------
    
    Co-authored-by: amaanlari <amaanlari@outlook.comm>

--- src/main/java/com/lari/bloggerhub/controller/blogpost/BlogPostController.java
+++ src/main/java/com/lari/bloggerhub/controller/blogpost/BlogPostController.java
@@ -4,7 +4,6 @@ import com.lari.bloggerhub.dto.request.BlogPostRequestDto;
 import com.lari.bloggerhub.response.Response;
 import com.lari.bloggerhub.service.blogpost.BlogPostService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,12 +12,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/blogposts")
 public class BlogPostController {
 
   private final BlogPostService blogPostService;
@@ -27,41 +24,37 @@ public class BlogPostController {
     this.blogPostService = blogPostService;
   }
 
-  @GetMapping("/{username}")
+  @GetMapping("/api/user/{username}/blogposts")
   public ResponseEntity<Response> getBlogPostsByUsername(@PathVariable String username) {
     return blogPostService.getAllBlogPostsByUsername(username);
   }
 
-  @GetMapping("/post/{id}")
-  public ResponseEntity<Response> getBlogPostById(
-      @PathVariable String id, Authentication authentication) {
+  @GetMapping("/api/blogposts/{id}")
+  public ResponseEntity<Response> getBlogPostById(@PathVariable String id, Authentication authentication) {
     return blogPostService.getBlogPostById(id, authentication);
   }
 
-  @PostMapping
+  @PostMapping("/api/blogposts")
   public ResponseEntity<Response> createBlogPost(
       @RequestBody BlogPostRequestDto blogPost, Authentication authentication) {
     return blogPostService.createBlogPost(blogPost, authentication);
   }
 
-  @PutMapping("/{id}")
+  @PutMapping("/api/blogposts/{id}")
   public ResponseEntity<Response> updateBlogPost(
-      @PathVariable String id,
-      @RequestBody BlogPostRequestDto updatedBlogPost,
-      Authentication authentication) {
+      @PathVariable String id, BlogPostRequestDto updatedBlogPost, Authentication authentication) {
     return blogPostService.updateBlogPost(id, updatedBlogPost, authentication);
   }
 
-  @DeleteMapping("/{id}")
+  @DeleteMapping("/api/blogposts/{id}")
   public ResponseEntity<Response> deleteBlogPost(
       @PathVariable String id, Authentication authentication) {
     return blogPostService.deleteBlogPost(id, authentication);
   }
 
-  @PreAuthorize("hasRole('PREMIUM_USER')")
   @PatchMapping("/{id}/premium")
   public ResponseEntity<Response> updateBlogPostPremiumStatus(
-      @PathVariable String id, @RequestParam(name = "is_premium") boolean isPremium) {
+      @PathVariable String id, @RequestParam boolean isPremium) {
     return blogPostService.updateBlogPostPremiumStatus(id, isPremium);
   }
 }
