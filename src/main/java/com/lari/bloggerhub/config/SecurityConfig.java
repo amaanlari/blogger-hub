2c9b14e86da2d9ddf2979047417da90e7e6e1291
amaanlari <amaanlari@outlook.comm>
2026-09-13 01:03:11 +0530

    Fixing vercel deployment CORS related bug

--- src/main/java/com/lari/bloggerhub/config/SecurityConfig.java
+++ src/main/java/com/lari/bloggerhub/config/SecurityConfig.java
@@ -142,7 +142,7 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("*"));
+    configuration.setAllowedOriginPatterns(List.of("*"));
     configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     configuration.setAllowCredentials(true);
     configuration.setAllowedHeaders(List.of("*"));
