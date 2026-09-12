6d608046b4427b172fb76f4fecbc2d200c8350f2
amaanlari <amaanlari@outlook.comm>
2026-09-13 00:08:42 +0530

    Add logging for MONGODB_DATABASE environment variable in main application

--- src/main/java/com/lari/bloggerhub/BloggerHubApplication.java
+++ src/main/java/com/lari/bloggerhub/BloggerHubApplication.java
@@ -1,5 +1,7 @@
 package com.lari.bloggerhub;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,8 +11,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication
 public class BloggerHubApplication {
 
+  private static final Logger log = LoggerFactory.getLogger(BloggerHubApplication.class);
+
   @SuppressWarnings("checkstyle:MissingJavadocMethod")
   public static void main(String[] args) {
+    System.out.println("MONGODB_DATABASE = [" + System.getenv("MONGODB_DATABASE") + "]");
+    log.warn("MONGODB_DATABASE = [" + System.getenv("MONGODB_DATABASE") + "]");
+
     SpringApplication.run(BloggerHubApplication.class, args);
   }
 }
